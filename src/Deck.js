f94399216110abc209e2eaa6abf1bb4c34ccc41e
michgboxy <michgboxy@gmail.com>
2020-11-12 13:20:25 +0100

    swipe

---
+++ src/Deck.js
@@ -0,0 +1,168 @@
+import React, {Component} from 'react';
+import {View,
+         Animated,
+         Dimensions,
+         LayoutAnimation,
+         UIManager, 
+         PanResponder} from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
+
+class Deck extends Component {
+    static defaultProps = {
+        onSwipeRight: () => {},
+        onSwipeLeft: () => {}
+    }
+
+
+
+
+    constructor(props){
+        super(props);
+
+        const position = new Animated.ValueXY();
+
+        const PanResponder2 = PanResponder.create({
+        
+        onStartShouldSetPanResponder: () => true, //make the component draggable
+        
+        
+        onPanResponderMove: (event, gesture) => {
+            position.setValue({x : gesture.dx, y: gesture.dy});
+        },
+        
+        
+        onPanResponderRelease: (event, gesture) => {
+            if(gesture.dx > SWIPE_THRESHOLD){
+                this.forceSwipe('right');
+            
+            }else if(gesture.dx < -SWIPE_THRESHOLD){
+                this.forceSwipe('left');
+            
+            }else {
+                this.resetPosition();
+            }
+
+
+            
+        }
+   });
+
+
+        this.state = {PanResponder2, position, index : 0};
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.data !== this.props.data){
+            this.setState({index : 0});
+        }
+
+    }
+
+    componentWillUpdate(){
+        UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true); //
+        LayoutAnimation.spring();// This tells RN to animate whatever whatever change in component on the next render
+    }
+
+    forceSwipe(direction){
+        const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+        Animated.timing(this.state.position, {
+            toValue: {x, y: 0},
+            duration: SWIPE_OUT_DURATION
+        }).start(() => this.onSwipeComplete(direction));
+    }
+
+    onSwipeComplete(direction){
+        const {onSwipeLeft, onSwipeRight, data} = this.props;
+        const item = data[this.state.index]
+
+        direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+
+        this.state.position.setValue({x:0, y: 0});
+        this.setState({index: this.state.index + 1});
+
+    }
+
+
+
+    resetPosition(){
+        Animated.spring(this.state.position, {
+            toValue: {x: 0, y : 0}
+        }).start();
+    }
+
+    getCardStyle(){
+        const {position} = this.state;
+        const rotate = position.x.interpolate({
+            inputRange:  [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+            outputRange : ['-120deg', '0deg', '120deg']
+        });
+
+
+        return {
+            ...this.state.position.getLayout(), //handles response movement to gesture //this.state.position.getLayout() updates the XY position of the card.
+            transform: [{rotate}] //handles rotational movement////input is the range of drag, output range is how distorted it is on the screen.
+        }  
+    }
+
+
+
+    
+
+    renderCards(){
+        if(this.state.index >= this.props.data.length) {
+            return this.props.renderNoMoreCards();
+        }
+
+
+
+        return this.props.data.map((item, i) => {
+            if(i < this.state.index){
+                return null;
+            }
+
+            if(i === this.state.index){
+                return (
+                    <Animated.View
+                       key={item.id}
+                        style={[this.getCardStyle(), styles.cardStyle]} //handles response movement to gesture //this.state.position.getLayout() updates the XY position of the card.
+                              {...this.state.PanResponder2.panHandlers} //handles the gesture
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                )
+
+            }
+
+            return (
+                <Animated.View 
+                    key={item.id} 
+                    style={[styles.cardStyle, {top : 10 * (i - this.state.index)}]}>
+                    {this.props.renderCard(item)}
+                </Animated.View>
+                
+            );
+        }).reverse();
+    }
+
+
+    render(){
+        return (
+            <View>
+                {this.renderCards()}
+            </View>
+            
+        );
+    }
+}
+
+const styles = {
+    cardStyle: {
+        position: 'absolute',
+        width: SCREEN_WIDTH
+    }
+}
+
+export default Deck;
